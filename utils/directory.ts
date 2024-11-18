8960aa43880f017516ea8818e42059b7be587a6c
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-11-18 12:21:06 +0200

    Designed a bit of database

---
+++ utils/directory.ts
@@ -0,0 +1,9 @@
+export function isDirectory(path: string):boolean {
+    try {
+		const stat = Deno.statSync(path);
+		if(stat.isDirectory) return true;
+        return false;
+	}catch (_err) {
+		return false;
+	}
+}
